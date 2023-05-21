6f61b2185e487c0f250fd2c35a8c303b1e78a3de
EdPaul1 <saidarajab101@gmail.com>
2023-05-21 18:27:48 +0300

    Inheritance in Ruby classes

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << String
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
